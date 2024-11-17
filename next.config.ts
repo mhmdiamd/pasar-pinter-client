8fff9c87bd663a80d57f4a3765b9f66aaa3115dc
mhmdiamd <m1pro2021@mhmdiamd-2.local>
2024-11-17 12:10:13 +0700

    ci: edit next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**",
       },
       {
         protocol: "https",
